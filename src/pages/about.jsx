8d57f9d190b2f6370f04641c3daa2f675a7b0297
unknown <jjayavardhan81@gmail.com>
2023-08-25 17:20:09 +0530

    new

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,7 +4,7 @@ function About() {
         <div>
             <div class="relative  overflow-hidden  bg-gradient-to-br  from-[hsl(0,0%,90%)] to-[hsl(0,0%,100%)] dark:from-[hsl(0,0%,6%)] dark:to-[hsl(0,0%,0%)]">
                 <section id="about" class=" py-10 md:py-20  relative space-y-16 p-6  justify-center items-center flex flex-col  " >
-                <p class="w-full  text-center items-center  mt-4 justify-center  text-4xl font-extrabold tracking-tigh dark:bg-gradient-to-b dark:from-white dark:to-[#adadad] bg-gradient-to-b from-[#555] to-[#000] text-transparent bg-clip-text">Let me Introduce<span class="text-blue-600"> MySelf</span></p>
+                <p class="w-full  text-center items-center  mt-4 justify-center  text-4xl font-extrabold tracking-tigh dark:text-white ">Let me Introduce<span class="text-blue-600"> MySelf</span></p>
                 <span class="text-white items-center justify-center text-center text-lg display:inline-block;vertical-align:top;text-decoration:inherit;text-wrap:balance">
                 Greetings to my digital domain where the fusion of design and code takes center stage...!<br />
 I go by the name <span class="text-xl text-white font-bold text-center tracking-normal hover:scale-125">Jayavardhan J</span>, a committed and versatile web developer and designer,<br /> fueled by an intense drive to create captivating digital journeys. Armed with a recent <br /> <span class="text-xl text-white font-bold  text-center tracking-normal hover:scale-125">Master's degree in Computer Applications (MCA)</span>, my venture into the realm of technology <br /> has been nothing short of enlightening and exhilarating.<br /><br />
